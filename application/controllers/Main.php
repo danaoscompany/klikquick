a1f0a9162afc22ef9033e042036e1e5f3768ef21
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-02-23 13:26:04 +0700

    a

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -32,7 +32,7 @@ class Main extends CI_Controller {
   
   public function get_by_id_name() {
     $this->load->helper('form');
-    $name = $this->input->get('name');
+    $name = $this->input->post('name');
     /*$idName = $this->input->post('id_name');
     $id = intval($this->input->post('id'));
     $query = $this->db->get_where($name, array(
